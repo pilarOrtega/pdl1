3d30d8ded17416f5f7157d83483e34acc4aa078e
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-04-01 13:57:39 +0200

    Initial code pdl1

---
+++ pdl1.py
@@ -0,0 +1,43 @@
+import os
+from openslide import OpenSlide, deepzoom
+import numpy
+from matplotlib import pyplot as plt
+import pysliderois.tissue as tissue
+
+slidefile1 = "/Users/pilarortega/Desktop/STAGE_ONCOPOLE/slides/NVA_RC.PDL1.V1_18T040165.2B.4963.PDL1.mrxs"
+slide1 = OpenSlide(slidefile1)
+
+image1 = slide1.read_region((0,0), 7, slide1.level_dimensions[7])
+image1 = numpy.array(image1)[:, :, 0:3]
+
+plt.figure(figsize=(7, 14))
+plt.imshow(image1)
+plt.show()
+
+image1_tejido_mask = tissue.get_tissue(image1, method = "rgb")
+#imagen1_tejido = image1*image1_tejido_mask
+
+plt.imshow(image1_tejido_mask)
+
+deepzoom1 = deepzoom.DeepZoomGenerator(slide1)
+
+level = 16
+PATH = "/Users/pilarortega/Desktop/STAGE_ONCOPOLE/level_{}".format(level)
+try:
+  os.mkdir(PATH)
+  print("Directory", PATH, "created")
+except FileExistsError:
+  print("Directory", PATH, "already exists")
+  
+tiles = deepzoom1.level_tiles[level]
+n=0
+for i in range(tiles[0]):
+  for j in range(tiles[1]):
+    tile = deepzoom1.get_tile(level,(i,j))
+    tile_path = PATH + '/{}_slide1_level{}_{}_{}.jpg'.format(n,level,i,j)
+    tile_bw = tile.convert(mode='L')
+    tile_bw = numpy.array(tile_bw)
+    x = tile_bw.mean()
+    if x < 250:
+      tile.save(tile_path)
+      n = n + 1
