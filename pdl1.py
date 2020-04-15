f74b800959bf30865bb3aa01c9bf52daa132f419
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-04-15 15:31:30 +0200

    Clustering methods - kmeans + dense + dab detection

--- pdl1.py
+++ pdl1.py
@@ -7,8 +7,10 @@ import sys
 import cv2
 from PythonSIFT import pysift
 import glob
-from skimage.io import sift
+from skimage.io import sift, imread
 from sklearn.cluster import MiniBatchKMeans
+from tqdm import tqdm
+from skimage.util.shape import view_as_windows
 
 def get_patches(slidepath, outpath, level = 10):
     """
@@ -52,32 +54,61 @@ def get_patches(slidepath, outpath, level = 10):
     print('Total of {} tiles in level {}'.format(n,level))
     return n, PATH
 
-def get_features(image_path, total):
+def get_features_SIFT(image_path, total):
     n=0
     des_list = []
     image_path = image_path + '/*.jpg'
     print('Obtaining SIFT features from images in ' + image_path + '...')
-    for im in glob.glob(image_path):
+    for im in tqdm(glob.glob(image_path)):
         image = cv2.imread(im, 0)
         n = n + 1
         keypoints, descriptors = pysift.computeKeypointsAndDescriptors(image)
-        print('Features obtained from image '+ str(n)+ ' of ' + str(total))
+        #print('Features obtained from image '+ str(n)+ ' of ' + str(total))
         des_list.append((im, descriptors))
-        if n%20 == 0:
-          sys.stdout.write(str(n)+" ")
-          sys.stdout.flush()
+        #if n%20 == 0:
+          #sys.stdout.write(str(n)+" ")
+          #sys.stdout.flush()
     # Stack all the descriptors vertically in a numpy array
     descriptors = des_list[0][1]
     for im, descriptor in des_list[1:]:
         descriptors = np.vstack((descriptors, descriptor))  # Stacking the descriptors
     return descriptors
 
+def get_features_dense(image_path, total):
+    image_path = image_path + '/*.jpg'
+    print('Obtaining dense features from images in ' + image_path + '...')
+    nclusters = 256
+    kmeans = MiniBatchKMeans(n_clusters = nclusters)
+    patch_shape = (8, 8, 3)
+    for im in tqdm(glob.glob(image_path)):
+        image = imread(im)
+        image = numpy.asarray(image)
+        image = image.astype(float)
+        patches = view_as_windows(image, patch_shape)
+        plines = patches.shape[0]
+        pcols = patches.shape[1]
+        patches_reshaped = patches.reshape(plines, pcols, patch_shape[0] * patch_shape[1] * patch_shape[2])
+        patches_reshaped = patches_reshaped.reshape(plines * pcols, patch_shape[0] * patch_shape[1] * patch_shape[2])
+        kmeans.partial_fit(patches_reshaped)
+
+    print('Obtaining histogram of features...')
+    for im in tqdm(glob.glob(image_path)):
+         image = imread(im)
+         image = numpy.asarray(image)
+         image = image.astype(float)
+         patches = view_as_windows(image, patch_shape)
+         plines = patches.shape[0]
+         pcols = patches.shape[1]
+         patches_reshaped = patches.reshape(plines, pcols, patch_shape[0] * patch_shape[1] * patch_shape[2])
+         patches_reshaped = patches_reshaped.reshape(plines * pcols, patch_shape[0] * patch_shape[1] * patch_shape[2])
+         frequence = kmeans.predict(patches_reshaped)
+         histogram =
 
 slidefile1 = "/Users/pilarortega/Desktop/STAGE_ONCOPOLE/slides/NVA_RC.PDL1.V1_18T040165.2B.4963.PDL1.mrxs"
 outpath = "/Users/pilarortega/Desktop/STAGE_ONCOPOLE/patches"
-n, outpath = get_patches(slidefile1, outpath, 13)
+n, outpath = get_patches(slidefile1, outpath, 15)
 print("Tiles are saved in " + outpath)
-lista = get_features(outpath, n)
+#lista = get_features(outpath, n)
 print("Features obtained. Proceding to KMeans clustering...")
 k = 100
 kmeans = MiniBatchKMeans(n_clusters=k)
