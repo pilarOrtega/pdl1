dca7cade1390b52f3f4a5e7c241a05233fce9eeb
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-04-30 08:56:39 +0200

    Style corrections

--- show_random_imgs.py
+++ show_random_imgs.py
@@ -5,9 +5,10 @@ import glob
 import argparse
 from skimage.io import sift, imread, imsave, imshow
 
-def show_random_imgs(images, x, y, figsize = (10, 10)):
+
+def show_random_imgs(images, x, y, figsize=(10, 10)):
     n = x * y
-    fig, axes = plt.subplots(x, y, figsize = figsize, sharex=True, sharey=True)
+    fig, axes = plt.subplots(x, y, figsize=figsize, sharex=True, sharey=True)
     image_paths = os.path.join(images, '*.jpg')
     im = glob.glob(image_paths)
     print('Number of images ' + str(len(im)))
@@ -28,11 +29,11 @@ def show_random_imgs(images, x, y, figsize = (10, 10)):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Shows a grid of size x*y displaying a random set of images from the defined path')
-    parser.add_argument('-p', '--path', type = str, required = True, help = 'path to image folder')
-    parser.add_argument('-x', '--x', type = int, required = True, help = 'number of images in x ')
-    parser.add_argument('-y', '--y', type = int, required = True, help = 'number of images in y ')
-    parser.add_argument('-f1', '--figsize1', type = int, default = 10, help = 'display size [Default: %(default)s]')
-    parser.add_argument('-f2', '--figsize2', type = int, default = 10, help = 'display size [Default: %(default)s]')
+    parser.add_argument('-p', '--path', type=str, required=True, help='path to image folder')
+    parser.add_argument('-x', '--x', type=int, required=True, help='number of images in x ')
+    parser.add_argument('-y', '--y', type=int, required=True, help='number of images in y ')
+    parser.add_argument('-f1', '--figsize1', type=int, default=10, help='display size [Default: %(default)s]')
+    parser.add_argument('-f2', '--figsize2', type=int, default=10, help='display size [Default: %(default)s]')
     args = parser.parse_args()
 
-    show_random_imgs(args.path, args.x, args.y, figsize = (args.figsize1, args.figsize2))
+    show_random_imgs(args.path, args.x, args.y, figsize=(args.figsize1, args.figsize2))
