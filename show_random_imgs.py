851b7acf99acc60a8aab984690fbd5f16bd67b13
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-04-30 11:29:30 +0200

    Change method to get image list as an argument

--- show_random_imgs.py
+++ show_random_imgs.py
@@ -9,19 +9,17 @@ from skimage.io import sift, imread, imsave, imshow
 def show_random_imgs(images, x, y, figsize=(10, 10)):
     n = x * y
     fig, axes = plt.subplots(x, y, figsize=figsize, sharex=True, sharey=True)
-    image_paths = os.path.join(images, '*.jpg')
-    im = glob.glob(image_paths)
-    print('Number of images ' + str(len(im)))
+    print('Number of images ' + str(len(images)))
     ax = axes.ravel()
-    if len(im) <= n:
-        for i in range(len(im)):
-            image = imread(im[i])
-            ax[i].imshow(image)
+    if len(images) <= n:
+        for i in range(len(images)):
+            im = imread(images[i])
+            ax[i].imshow(im)
     else:
         for i in range(n):
-            k = randint(0,len(im)-1)
-            image = imread(im[k])
-            ax[i].imshow(image)
+            k = randint(0, len(images)-1)
+            im = imread(images[k])
+            ax[i].imshow(im)
     fig.tight_layout()
     plt.show()
 
@@ -29,11 +27,13 @@ def show_random_imgs(images, x, y, figsize=(10, 10)):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Shows a grid of size x*y displaying a random set of images from the defined path')
-    parser.add_argument('-p', '--path', type=str, required=True, help='path to image folder')
+    parser.add_argument('-p', '--path', type=str, help='path to image folder')
     parser.add_argument('-x', '--x', type=int, required=True, help='number of images in x ')
     parser.add_argument('-y', '--y', type=int, required=True, help='number of images in y ')
     parser.add_argument('-f1', '--figsize1', type=int, default=10, help='display size [Default: %(default)s]')
     parser.add_argument('-f2', '--figsize2', type=int, default=10, help='display size [Default: %(default)s]')
     args = parser.parse_args()
 
-    show_random_imgs(args.path, args.x, args.y, figsize=(args.figsize1, args.figsize2))
+    image_paths = os.path.join(args.path, '*.jpg')
+    image_list = glob.glob(image_paths)
+    show_random_imgs(image_list, args.x, args.y, figsize=(args.figsize1, args.figsize2))
