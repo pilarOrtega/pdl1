2e55ea0d57a6764a4f84218149c5e255aab170ac
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-05-19 14:38:27 +0200

    Changed image title

--- show_random_imgs.py
+++ show_random_imgs.py
@@ -23,7 +23,8 @@ def show_random_imgs(images, x, y, figsize=(10, 10), save_fig=False, name=''):
             image_slide = image_data.split('-')[0]
             im = imread(images[k])
             ax[i].imshow(im)
-            ax[i].set_title(image_slide+image_number, fontdict={'fontsize': 6, 'fontweight': 'medium'})
+            ax[i].set_title(image_slide+'-'+image_number, fontdict={'fontsize': 6, 'fontweight': 'medium'})
+    fig.suptitle(name)
     fig.tight_layout()
     plt.show()
     if save_fig:
