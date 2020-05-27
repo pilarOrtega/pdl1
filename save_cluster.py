aa57e06fafa3a29fdcd2c9086e0ab7ed1ce09d82
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-05-27 12:22:54 +0200

    Adapted to new patch file name including #

--- save_cluster.py
+++ save_cluster.py
@@ -46,8 +46,9 @@ def get_clusterlist(outpath, classifier, n_division):
     print('Get cluster list from {}'.format(outpath))
     for im in tqdm(image_list):
         image_name = os.path.basename(im)
+        image_name = image_name.split('#')[1]
         number = image_name.split('-')
-        number = int(number[1])
+        number = int(number[0])
 
         if classifier[number][3] == 0:
             continue
