55fec9a4b7f25461b5eedb46ad382bd0039b1609
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-09 15:21:53 +0200

    test

--- feature_extraction.py
+++ feature_extraction.py
@@ -273,7 +273,7 @@ def get_features_CNN(image_list, outpath, model='VGG16'):
         print('Loading network...')
 
         outdir = os.path.join(outpath, model)
-        ft.domain_adaption(image_list, outdir, 224)
+        ft.domain_adaption(image_list, outdir, "1", 224)
         weights_dir = os.path.join(outdir, 'weights')
         model = load_model(outdir, 5)
         model.summary()
