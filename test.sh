5fdd0dc075fe1bfd14f4ffbd1ee65c9fa27c7e9c
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-15 16:23:34 +0200

    Change shell script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python test.py -s '/home/DeepLearning/media/pdl1/RC.PDL1.V2' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3' -f 'DenseDAB' -m 'BottomUp' --flag 3 -j -2 -c 23| tee Records/test3_DenseDAB_BU_1506-6.txt
-python test.py -s '/home/DeepLearning/media/pdl1/RC.PDL1.V2' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3' -f 'DaisyDAB' -m 'BottomUp' --flag 3 -j -2 -c 23| tee Records/test3_DaisyDAB_BU_1506-1.txt
+python test.py -s '/home/DeepLearning/media/pdl1/RC.PDL1.V2' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3' -f 'DaisyDAB' -m 'BottomUp' --flag 2 -j -2 -c 23| tee Records/test3_DaisyDAB_BU_1506-1.txt
