ca865e76e50cc82ef9d03548f741c8f2a5239901
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-17 08:37:50 +0200

    Solved error nclusters

--- cluster_division.py
+++ cluster_division.py
@@ -57,7 +57,7 @@ def image_cluster(features, classifiers, n, method='Kmeans'):
     return classifiers, features_1, features_0
 
 
-def cluster_division(features, classifiers_0, n_division, outpath, feature_method, method='TopDown', nclusters=16, save=False):
+def cluster_division(features, classifiers_0, n_division, outpath, feature_method, method='TopDown', ncluster=16, save=False):
     """
     Arguments:
         - features:
@@ -186,4 +186,4 @@ if __name__ == "__main__":
     feature_method = os.path.splitext(feature_method)[0]
     feature_method = feature_method.split('_')[1]
 
-    classifiers = cluster_division(features, classifiers, n_division, outpath, feature_method, method = args.method, nclusters=args.nclusters)
+    classifiers = cluster_division(features, classifiers, n_division, outpath, feature_method, method = args.method, nclustes=args.nclusters)
