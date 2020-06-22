9ee48feefc803038d1ee58c61bced2760f703826
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-22 16:21:54 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-for i in 71 72 73 74 75 76 77 78 79 80
-do
-  python cluster_division.py -f '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/features_DenseDAB_level16.p' -c '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -m 'BottomUp' --nclusters 23| tee Records/test3_DenseDAB_BU_2206-$i.txt
-  mv /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseDAB-16-BottomUp.p /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseDAB-16-BottomUp-$i.p
-done
-
 python feature_extraction.py -l '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/list_positive_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -f 'DenseH'
 
 for i in 81 82 83 84 85 86 87 88 89 90
