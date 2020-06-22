b164045ffdb639560d715da9e4c4c833773bb033
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-22 08:47:02 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-python detect_dab.py -l '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/list_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -t 10
-python feature_extraction.py -l '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/list_positive_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -f 'DenseDAB'
 
 for i in 71 72 73 74 75 76 77 78 79 80
 do
-  python cluster_division.py -f '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/features_DenseDAB_level16.p' -c '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -m 'BottomUp' --nclusters 23| tee Records/test3_DaisyH_BU_1806-$i.txt
-  mv /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class-DenseDAB-16-BottomUp.p /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class-DenseDAB-16-BottomUp-$i.p
+  python cluster_division.py -f '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/features_DenseDAB_level16.p' -c '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -m 'BottomUp' --nclusters 23| tee Records/test3_DenseDAB_BU_2206-$i.txt
+  mv /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseDAB-16-BottomUp.p /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseDAB-16-BottomUp-$i.p
 done
 
-python feature_extraction.py -l '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/list_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -f 'DenseH'
+python feature_extraction.py -l '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/list_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -f 'DenseH'
 
 for i in 81 82 83 84 85 86 87 88 89 90
 do
   python cluster_division.py -f '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/features_DenseH_level16.p' -c '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class_16_224.p' -o '/data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4' -m 'BottomUp' --nclusters 23| tee Records/test3_DaisyH_BU_1806-$i.txt
-  mv /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class-DenseH-16-BottomUp.p /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test3/class-DenseH-16-BottomUp-$i.p
+  mv /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseH-16-BottomUp.p /data/DeepLearning/ABREU_Arnaud/Pilar_stage/tests/level_16_ts_224_V2_test4/class-DenseH-16-BottomUp-$i.p
 done
