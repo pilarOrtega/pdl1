685e2344ae2e55ff95dacb8afa6a1e83a924234f
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-06-24 11:16:47 +0200

    minor error exception

--- test.py
+++ test.py
@@ -36,8 +36,8 @@ if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        except RuntimeError as e:
-            print(e)
+    except RuntimeError as e:
+        print(e)
 
 slides = args.slides
 outpath = args.outpath
