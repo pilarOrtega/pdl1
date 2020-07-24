0d40d5e22bdeaac3c836e24c8241023734d781a9
Pilar Ortega <pilar.ortega.arevalo@gmail.com>
2020-07-24 16:13:21 +0200

    New weighted method color dictionary

--- weighted_clustering.py
+++ weighted_clustering.py
@@ -20,13 +20,20 @@ def obtain_init_array(list_patches, features, model):
 
 def weighted_clustering(data, features, outpath, feature_method, classifiers, slide_folder, model):
     labels = read_csv(data)
-    labels_dict = {(int(label[0])+2): label[2] for label in labels}
-    color_dict = './dict/color_dict.csv'
-    color_dict = read_csv(color_dict)
-    color_dict = {(int(c[0])): (float(c[1]), float(c[2]), float(c[3])) for c in color_dict}
+    labels_dict = {}
     labels_dict[0] = 'Background'
     labels_dict[1] = 'Negative'
-    data = [label[1] for label in labels]
+    label_color_dict = {}
+    data = []
+    for label in labels:
+        labels_dict[(int(label[0])+2)] = label[2]
+        label_color_dict[(int(label[0])+2)] = (float(label[3]), float(label[4]), float(label[5]))
+        data.append(label[1])
+    init_color_dict = './dict/color_dict.csv'
+    init_color_dict = read_csv(init_color_dict)
+    init_color_dict = {(int(c[0])): (float(c[1]), float(c[2]), float(c[3])) for c in init_color_dict}
+    label_color_dict[0] = init_color_dict[0]
+    label_color_dict[1] = init_color_dict[1]
 
     init_arr = obtain_init_array(data, features, model)
     features_mod = []
@@ -40,32 +47,39 @@ def weighted_clustering(data, features, outpath, feature_method, classifiers, sl
     for c in classifiers:
         preview.append(get_preview(c[0], c[2], 16, 224, slide_folder, 4, method='BottomUp'))
 
-    unique_labels = [(color_dict[key], labels_dict[key]) for key in labels_dict]
-    #unique_labels = set(unique_labels)
+    unique_labels = [(label_color_dict[key], labels_dict[key]) for key in labels_dict]
+    unique_labels = set(unique_labels)
     patchList = []
     for patch in unique_labels:
         data_key = mpatches.Patch(color=patch[0], label=patch[1])
         patchList.append(data_key)
 
+    n_label_clusters = len(data)
+    n_unlabel_clusters = model.cluster_centers_.shape[0]
+    n_complet = n_label_clusters + n_unlabel_clusters + 2
+    color_dict = {}
+    for i in range(n_complet):
+        if i < (n_label_clusters+2):
+            color_dict[i] = label_color_dict[i]
+        else:
+            false_i = len(init_color_dict)-(i-n_label_clusters)
+            color_dict[i] = init_color_dict[false_i]
+            data_key = mpatches.Patch(color=init_color_dict[false_i], label='Cluster {}'.format(i-2))
+            patchList.append(data_key)
+
+
+    fig = plt.figure()
+    spec = gridspec.GridSpec(ncols=1, nrows=1)
+    ax0 = fig.add_subplot(spec[0])
+    ax0.legend(handles=patchList, loc='center left', prop={'size': 15})
+    ax0.axis("off")
+    name = os.path.join(outpath, 'legend_'+feature_method+'.png')
+    fig.savefig(name, bbox_inches='tight', dpi=fig.dpi)
+
     for im in preview:
         image = numpy.array([[color_dict[x] for x in row] for row in im[1]])
         image2 = numpy.transpose(image, (1, 0, 2))
         plt.imsave(os.path.join(outpath,'cmap_{}_{}.png'.format(im[0], feature_method)), image2)
-
-        fig = plt.figure(figsize=(13, 4))
-        spec = gridspec.GridSpec(ncols=2, nrows=1, width_ratios=[2, 1])
-        ax0 = fig.add_subplot(spec[0])
-        ax1 = fig.add_subplot(spec[1])
-
-        ax0.imshow(image)
-        ax1.legend(handles=patchList, loc='center left', prop={'size': 15})
-        ax1.axis("off")
-        title = im[0] + ' ' + feature_method
-        fig.suptitle(title, va='baseline', fontsize=15)
-        fig.tight_layout()
-        plt.show()
-        name = os.path.join(outpath, im[0]+feature_method+'.png')
-        fig.savefig(name, bbox_inches='tight', dpi=fig.dpi)
 
     return classifiers
 
